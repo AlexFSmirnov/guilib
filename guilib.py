b590b8676bf245b02d3470655a985f542c0e6f68
AlexFSmirnov <alex.f.smirnov@yandex.ru>
2016-07-06 23:39:16 +0300

    Added some workpieces for InputFields.

--- guilib.py
+++ guilib.py
@@ -1,32 +1,65 @@
 import pygame 
 
 
+# This class creates an in
+#
+#class inputField:
+    #def __init__(x, y,
+                 #textlen,
+                 #size,
+                 #font_path               = "fonts/en.png",
+                 #background_color        = (255, 255, 255),
+                 #background_color_active = (255, 255, 255),
+                 #outline_color           = (100, 100, 100),
+                 #outline_color_acive     = (255, 0, 0),
+                 #outline_width           = 2):
+        #pass
+
+#def inputField(surface,
+               #x, y,
+               #textlen, 
+               #sym_size,
+               #resizable        = True,
+               #font_path        = "fonts/en.png",
+               #background_color = (255, 255, 255),
+               #outline_color    = (100, 100, 100),
+               #outline_width    = 2): 
+    #is_on = True
+    #while is_on:
+        #for e in pygame.event.get():
+            #if e.type == pygame.QUIT:
+                #pygame.quit()
+                #quit(0)
+            
+               
+
+
 # This function returns a pygame.Surface object 
 # with a given text drawn on it.
 def textSurface(text,
-                size,
-                font_path       = "fonts/en.png",
-                background_color= (255, 255, 255),
-                outline_color   = (100, 100, 100),
-                outline_width   = 2):
+                sym_size,
+                font_path        = "fonts/en.png",
+                background_color = (255, 255, 255),
+                outline_color    = (100, 100, 100),
+                outline_width    = 2):
     text = text.split('\n')
     text_w = max([len(line) for line in text])
     text_h = len(text)
     
-    text_surf = pygame.Surface((size *  text_w + 2 * outline_width,
-                                            size * text_h + 2 * outline_width))
+    text_surf = pygame.Surface((sym_size *  text_w + 2 * outline_width,
+                                            sym_size * text_h + 2 * outline_width))
     text_surf.fill(outline_color)
     pygame.draw.rect(text_surf, background_color, 
                      (outline_width, outline_width, 
-                              size * text_w, size * text_h))
+                              sym_size * text_w, sym_size * text_h))
     
     for line_idx, line in enumerate(text):
         for sym_idx, sym in enumerate(line):
             Basics.drawSym(text_surf,
                            sym,
-                           outline_width + sym_idx * size,
-                           outline_width + line_idx * size,
-                           size,
+                           outline_width + sym_idx * sym_size,
+                           outline_width + line_idx * sym_size,
+                           sym_size,
                            font_path)
     return text_surf
     
@@ -36,7 +69,7 @@ def textSurface(text,
 
 
 class Basics:
-    def drawSym(surface, sym, x, y, size=16, font_path="fonts/en.png"):
+    def drawSym(surface, sym, x, y, sym_size=16, font_path="fonts/en.png"):
         font = pygame.image.load(font_path)
         alph = ("abcdefghijklmnopqrstuvwxyz-0123456789" + 
                                    "ABCDEFGHIJKLMNOPQRSTUVWXYZ_)!@#$%^&*(.,/ ")
@@ -44,6 +77,6 @@ class Basics:
         
         idx = alph.index(sym)
         # Scaling the font file to size
-        font = pygame.transform.scale(font, (len(alph) * size, size)) 
+        font = pygame.transform.scale(font, (len(alph) * sym_size, sym_size)) 
         # Slicing the font file, so that only one symbol is left        
-        surface.blit(font, (x, y), (size * idx, 0, size, size)) 
+        surface.blit(font, (x, y), (sym_size * idx, 0, sym_size, sym_size)) 
--- tmp.py
+++ tmp.py
@@ -7,7 +7,8 @@ import sys
 
 pygame.init()
 
-screen = pygame.display.set_mode((500, 500))
+screen = pygame.display.set_mode((500, 500), HWSURFACE|DOUBLEBUF|RESIZABLE)
+display_surface = Surface((500, 500))
 
 
 while True:
@@ -15,10 +16,19 @@ while True:
         if e.type == QUIT:
             pygame.quit()
             sys.exit(0)
+        if e.type==VIDEORESIZE:
+            screen=pygame.display.set_mode(e.dict['size'], HWSURFACE|DOUBLEBUF|RESIZABLE)            
+            pygame.display.update()        
         
         if e.type == KEYDOWN:
             if e.key == K_q:
-                surf = guilib.textSurface("LoooKKK, \n  THIS IS A TEXT SURFACE!!!", 16)
-                
-                screen.blit(surf, (100, 100))
-                pygame.display.update()
+                surf = guilib.textSurface("LoooKKK, \n  TEXT SURFACE", 30)
+                display_surface.blit(surf, (30, 30))
+        
+        screen.blit(pygame.transform.scale(display_surface, 
+                                           (pygame.display.Info().current_w,
+                                            pygame.display.Info().current_h)), 
+                    (0, 0))
+        pygame.display.update()
+        
+        
